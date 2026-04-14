b8650479200dfda26858d85c3fcc6e359cbab014
SANAI-29 <askozlov954@gmail.com>
2026-04-14 15:17:52 +0400

    Update main.py

--- app/client/main.py
+++ app/client/main.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('Glava.html')
 
-@app.route('/about')
+
+@app.route('/about', methods=["GET", "POST"])
 def about():
+    if request.method == "POST":
+        print(request.form)
+
     return "это будет вторая страница"
 
 if __name__ == "__main__":
